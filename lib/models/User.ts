448f4442bfdea8e4ae7b4f29c8684fe2b283bde2
globalearn-app <irediagoodluck26@gmail.com>
2026-05-20 07:35:42 +0100

    Update User.ts

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -10,7 +10,7 @@ export interface IUser extends Document {
   country?: string;
   avatar?: string;
   role: "user" | "admin";
-  tier: 1 | 2 | 3 | 4 | 5 | 6;
+  plan: 1 | 2 | 3 | 4 | 5 | 6;
   isVerified: boolean;
   isActive: boolean;
   kycStatus: "pending" | "submitted" | "approved" | "rejected";
@@ -61,7 +61,7 @@ const UserSchema = new Schema<IUser>(
       enum: ["user", "admin"],
       default: "user",
     },
-    tier: {
+    plan: {
       type: Number,
       enum: [1, 2, 3, 4, 5, 6],
       default: 1,
